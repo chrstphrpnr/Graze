1cceddc02ea48fe536ede2ae9c9976637aaae872
Christopher Panerio <christopherpaneriotup@gmail.com>
2024-05-28 16:27:28 +0800

    Updates

--- app/src/main/java/com/example/soulscrypt/Constant/API.java
+++ app/src/main/java/com/example/soulscrypt/Constant/API.java
@@ -2,8 +2,8 @@ package com.example.soulscrypt.Constant;
 
 public class API {
 
-//    public static String URL = "https://soulscrypt.cloud/";
-    public static String URL = "http://192.168.8.69:8000/";
+    public static String URL = "https://soulscrypt.cloud/";
+//    public static String URL = "http://192.168.8.69:8000/";
 
     public static String API = URL + "api/";
 
